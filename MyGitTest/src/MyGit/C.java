8b919393258741b8cb3917e7a77d3eb65bf4300b
이진욱 <@192.168.219.105>
2024-12-23 20:25:44 +0900

    Modification A.java and C.java in devC

--- MyGitTest/src/MyGit/C.java
+++ MyGitTest/src/MyGit/C.java
@@ -5,6 +5,7 @@ public class C {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello C!");
+		System.out.println("modify C.java in devC");
 	}
 
 }
