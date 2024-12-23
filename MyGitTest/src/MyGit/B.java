2dc34be2ad3f25a56b5cbc5fcf776ff2a8be24a0
이진욱 <@192.168.219.105>
2024-12-23 20:24:19 +0900

    Modification A.java and B.java in devB

--- MyGitTest/src/MyGit/B.java
+++ MyGitTest/src/MyGit/B.java
@@ -5,6 +5,7 @@ public class B {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello B!");
+		System.out.println("modify B.java in devB!");
 	}	
 
 }
